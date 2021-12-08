df174d75df1930dd0db5049dbc9f59e8acd5fe25
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2021-12-08 15:15:44 +0100

    Add class Person

--- Person.cs
+++ Person.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace OOP
 {
-    class Person
+    public class Person
     {
+        private int Age { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private int Heigth { get; set; }
+        private int Weight { get; set; }
+        
     }
 }
