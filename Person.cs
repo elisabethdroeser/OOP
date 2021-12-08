6693f538ff977c0d47c25cc048fa8f4d0869c3f8
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2021-12-08 18:28:00 +0100

    Add exception

--- Person.cs
+++ Person.cs
@@ -1,24 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OOP
+﻿namespace OOP
 {
     public class Person
     {
-        public uint Age { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public uint Heigth { get; set; }
-        public uint Weight { get; set; }   
-  
-        //Add constructor to class
+        private int Age
+        {
+            get
+            {
+                return Age;
+            }
+            set
+            {
+                Age = ValidateAge(value);
+            }
+        }
+        //public string FirstName { get; set; }
+        private string FirstName
+        {
+            get
+            {
+                return FirstName;
+            }
+            set
+            {
+                FirstName = value;
+            }
+        }
+        private string LastName { get; set; }
+        private int Heigth { get; set; }
+        private int Weight { get; set; }
+
+        //Add constructor to class with required fields
         public Person(string fName, string lName)
         {
             FirstName = fName;
-            LastName = lName; 
+            LastName = lName;
+        }
+
+        public void ValidateAge(int Age)
+        {
+            if (Age > 0)
+            {
+                throw new ArgumentException("The age is high enough");
+            }
+            else 
+            {
+                Console.WriteLine("you need to add a new age");
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP
 {
@@ -6,6 +7,18 @@ namespace OOP
     {
         static void Main(string[] args)
         {
+
+            Person person = new Person("Lisa", "Droeser");
+
+            try
+            {
+
+            }
+            catch ArgumentException e);
+            {
+                Console.WriteLine(e.Message); 
+            }
+
             Console.ReadLine();
         }
     }
