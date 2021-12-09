88a7f8735425d39c22b9a39d11638ebf193d7891
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2021-12-09 18:50:27 +0100

    Create abstract class

---
+++ Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    abstract class Animal
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+        private int Weight { get; set; } 
+
+    }
+}
