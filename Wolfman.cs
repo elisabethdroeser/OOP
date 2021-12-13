c6b044427906a1e923bdc24eba639061f409e988
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2021-12-13 17:23:04 +0100

    Added inheritance with method Talk.

---
+++ Wolfman.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Wolfman : Wolf, IPerson
+    {
+        public Wolfman(string Name, int Age, int Weight, string Color) : base(Name, Age, Weight, Color)
+        {
+            
+        }
+        public void Talk()
+        {
+            Console.WriteLine("Come closer, I cannot see you enough!");
+        }
+    }
+}
